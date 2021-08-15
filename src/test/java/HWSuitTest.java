7d6ced9325f3742fbe2b8531d90c9753aa32998e
Yevgeniia Motytskaya <qayevheniia@gmail.com>
2021-08-15 19:52:05 +0300

    Added in  HW regression testing

--- src/test/java/HWSuitTest.java
+++ src/test/java/HWSuitTest.java
@@ -5,60 +5,152 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 
 public class HWSuitTest {
 
     private final String baseUrl = "https://lms.ithillel.ua/auth";
-    WebDriver driver;
-    WebDriverWait wait;
+    private final String expLinkAfterSignIn = "https://lms.ithillel.ua/";
+    private final String expLinkSupport = "https://t.me/hillel_bot";
+
     private final String emailValid = "qayevheniia@gmail.com";
     private final String passwordValid = "Mzhenya1987)";
+    private final String emailInvalid = "QA1212122@mvm.com";
+    private final String passwordInvalid = "232131";
+    String expAlertInvalidLogin = "Incorrect email or password";
 
 
-    @BeforeTest (groups = { "smoke", "regression" })
+//    the general wait-locators for pages
+    String waitElementAfterSignIn = "//*[contains(text(), ' QA Automation ')]";
+    String waitSupportIcon = "span.login__support-icon";
+    String waitElementBaseUrl = ".button.button--light-blue.button--simple.ng-star-inserted";
+    String waitAlertWrongCredential = ".validation-messages__item.ng-star-inserted";
 
-    public void downloadDriver(){
+    By emailField = (By.xpath("//input[@type='email']"));
+    By passwordField = (By.xpath("//input[@type='password']"));
+    By submitButtonSignIn = (By.cssSelector(".button--light-blue.button--simple.ng-star-inserted"));
+    By supportIcon = (By.cssSelector("span.login__support-icon"));
+    By logOutButton = (By.xpath("//*[contains(text(), ' Выйти ')]"));
+    By alertInvalidLogin = (By.cssSelector(".validation-messages__item.ng-star-inserted"));
+
+
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public void openBaseUrl() {
+        driver.get(baseUrl);
+    }
+
+    public void login(String email, String password) {
+        waitElement(waitElementBaseUrl);
+        driver.findElement(emailField).sendKeys(email);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(submitButtonSignIn).submit();
+
+    }
+
+    public void switchToLastWindow() {
+        for (String winHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(winHandle);
+        }
+    }
+
+    public void waitElement(String element) {
+        wait.until(ExpectedConditions.or(ExpectedConditions.visibilityOfElementLocated(By.xpath(element))
+                , ExpectedConditions.visibilityOfElementLocated(By.cssSelector(element))));
+    }
+
+//так как не срабатывала команда очистики куки пришлось сделать before
+// и after метода так как третий тест открывался сразу с залогениной страницы
+//    так можно сделать или это замедляет прохождение тестов?
+
+    @BeforeMethod(groups = {"smoke", "regression"})
+    public void downloadDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
+        driver.manage().deleteAllCookies();
+
     }
 
-    @AfterTest (groups = { "smoke", "regression" })
-    public void cleanDriver() {
+    @AfterMethod(groups = {"smoke", "regression"})
+   public void cleanDriver() {
         driver.quit();
     }
 
     @Test(groups = "regression")
     public void openUrl() {
-        driver.get(baseUrl);
+        openBaseUrl();
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://lms.ithillel.ua/auth", "Wrong url");
+        Assert.assertEquals(currentUrl, baseUrl, "Wrong url");
     }
 
-    @Test(groups = { "smoke", "regression" })
+
+    @Test(groups = {"smoke", "regression"})
     public void signIn() {
-        driver.get(baseUrl);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.login")));
-        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(emailValid);
-        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(passwordValid);
-        driver.findElement(By.cssSelector(".button--light-blue.button--simple.ng-star-inserted")).submit();
-        String currentTitle = driver.getTitle();
-        System.out.println(currentTitle);
-        Assert.assertTrue(currentTitle.contains("Система личных кабинетов Студентов Компьютерной школы Hillel"));
-    }
-
-    @Test(groups = { "smoke", "regression" })
-    public void signOut(){
-
+        openBaseUrl();
+        login(emailValid, passwordValid);
+        waitElement(waitElementAfterSignIn);
+        String currentlinkSignIn = driver.getCurrentUrl();
+        Assert.assertEquals(currentlinkSignIn, expLinkAfterSignIn, "Wrong support link");
     }
 
 
-// Будем ли мы выносить селектора в отдельный класс?
-// если у нас один и тот же код, например логинизация повторяется несколько раз, то нам вынести это в отдельный класс или как мы можем достучаться до метода, чтоб не дублировать код
-// если у нас одно непрерывное флоу  и нет возможности разбить на методы как быть в этом случае
-    
-}
+    @Test(groups = {"smoke", "regression"})
+    public void verifySupportIcon() {
+        openBaseUrl();
+        System.out.println(driver.getCurrentUrl());
+        waitElement(waitSupportIcon);
+        driver.findElement(supportIcon).click();
+        switchToLastWindow();
+        String currentlinkSupport = driver.getCurrentUrl();
+        Assert.assertEquals(currentlinkSupport, expLinkSupport, "Wrong support link");
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void logOut() {
+        openBaseUrl();
+        login(emailValid, passwordValid);
+        waitElement(waitElementAfterSignIn);
+        driver.findElement(logOutButton).click();
+        waitElement(waitElementBaseUrl);
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, baseUrl, "SignOut is broken");
+    }
+
+
+    @Test(groups = {"regression"})
+    public void InvalidEmailPassword(){
+        openBaseUrl();
+        login(emailInvalid, passwordInvalid);
+        waitElement(waitAlertWrongCredential);
+        String actAlertInvalidLogin = driver.findElement(alertInvalidLogin).getText();
+        Assert.assertEquals(actAlertInvalidLogin, expAlertInvalidLogin, "Wrong alert for invalid email & password");
+        String actUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actUrl, baseUrl, "Wrong url after sign in with wrong credential");
+    }
+    @Test(groups = {"regression"})
+    public void InvalidEmail(){
+        openBaseUrl();
+        login(emailInvalid, passwordValid);
+        waitElement(waitAlertWrongCredential);
+        String actAlertInvalidLogin = driver.findElement(alertInvalidLogin).getText();
+        Assert.assertEquals(actAlertInvalidLogin, expAlertInvalidLogin, "Wrong alert for invalid email");
+        String actUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actUrl, baseUrl, "Wrong url after sign in with wrong email");
+    }
+    @Test(groups = {"regression"})
+    public void InvalidPassword(){
+        openBaseUrl();
+        login(emailValid, passwordInvalid);
+        waitElement(waitAlertWrongCredential);
+        String actAlertInvalidLogin = driver.findElement(alertInvalidLogin).getText();
+        Assert.assertEquals(actAlertInvalidLogin, expAlertInvalidLogin, "Wrong alert for invalid email");
+        String actUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actUrl, baseUrl, "Wrong url after sign in with wrong password");
+    }
+    }
+
+
--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -8,7 +10,10 @@ public class UiTest {
 
     private final String baseUrl = "https://www.ikea.com/ua/uk/1213";
     WebDriver driver;
-
+    String  STANDART_USER_LOGIN ="standard_user";
+    String  STANDART_USER_PASSWORD ="secret_sauce";
+    String  LOCKER_USER_LOGIN ="locked_out_user";
+    String expectedResult = "https://www.saucedemo.com/inventory.html";
 
 
     @BeforeTest
@@ -57,5 +62,55 @@ public class UiTest {
         String currentTitle = driver.getTitle();
         System.out.println(currentTitle);
         Assert.assertTrue(currentTitle.contains("IKEA"));
+    }
+
+    private void login(String STANDART_USER_LOGIN, String STANDART_USER_PASSWORD){
+    WebElement loginStandart = driver.findElement(By.id("user-name"));
+    WebElement passwordFiled = driver.findElement(By.id("password"));
+    loginStandart.sendKeys(STANDART_USER_LOGIN);
+    passwordFiled.sendKeys(STANDART_USER_PASSWORD);
+    WebElement loginButton = driver.findElement(By.id("login-button"));
+    loginButton.click();
+}
+
+    @Test
+    public void standartUserLoginTest(){
+        openSource();
+        login(STANDART_USER_LOGIN, STANDART_USER_PASSWORD);
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, expectedResult,"User didn't login");
+    }
+
+
+    @Test
+    public void lockedtUserLoginTest() throws InterruptedException {
+        openSource();
+        login(LOCKER_USER_LOGIN, STANDART_USER_PASSWORD);
+        WebElement text = driver.findElement(By.cssSelector(".error-message-container.error"));
+        Thread.sleep(500);
+        String actualResult = text.getText();
+        System.out.println(text.getText());
+        String expectedResult = "Epic sadface: Sorry, this user has been locked out.";
+        Assert.assertEquals(actualResult,expectedResult,"Wrong result");
+    }
+
+    @Test
+    public void logOut() throws InterruptedException {
+        openSource();
+        login(STANDART_USER_LOGIN, STANDART_USER_PASSWORD);
+        WebElement burgerMenu = driver.findElement(By.id("react-burger-menu-btn"));
+        burgerMenu.click();
+        WebElement logOutButton = driver.findElement(By.id("logout_sidebar_link"));
+        Thread.sleep(500);
+        logOutButton.click();
+        Assert.assertFalse(driver.getCurrentUrl().contains(expectedResult));
+
+
+    }
+
+    public void openSource(){
+        driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
     }
 }
